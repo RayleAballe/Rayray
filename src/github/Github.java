82d6de062151c41f7473ec1da6c117d0cc19fff3
Student <Student@CLB3Unit26>
2024-02-07 15:05:46 +0800

    version 2

--- src/github/Github.java
+++ src/github/Github.java
@@ -15,7 +15,10 @@ public class Github {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World" ); 
+        int a = 1;
+        System.out.println(a);
+               
     }
     
 }
